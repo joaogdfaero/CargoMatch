8e862f3b3c2bbb9b042e00ea7c6db573edd28521
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-07-08 10:35:07 -0300

    feat: header and footer

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+    def sobre
+    end
+  
+    def contato
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :trips
   resources :users
   root to: 'pages#home'
+  get 'sobre', to: 'static_pages#sobre'
+  get 'contato', to: 'static_pages#contato'
 end
